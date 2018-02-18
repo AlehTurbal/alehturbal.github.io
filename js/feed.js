85d1b9d8415fddb402c486f683fd9a312657b4c1
Aleh <bisnomen@gmail.com>
2018-02-18 17:50:34 +0300

    add babel preloader

--- js/feed.js
+++ js/feed.js
@@ -542,39 +542,24 @@ module.exports = __webpack_require__(2);
 /* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
+"use strict";
+
+
 var Promise = __webpack_require__(3).Promise;
 
-window.fetch('https://newsapi.org/v1/articles?source=bbc-news&apiKey=7b670b164aa941c88eef1d1350fa4296')
-  .then(response => response.json())
-  .then(function(response) {
-    const { articles } = response;
-    let content = '';
-    articles.forEach(function(article) {
-      const date = new Date(article.publishedAt);
-      
-      content += `
-        <div class="row mt-3">
-          <div class="media">
-            <div class="col-4">
-              <a href=${article.url}>
-                <img src=${article.urlToImage} width='100%'>
-              </a>
-            </div> 
-            <div class="media-body col-8">
-              <h5>${article.title}</h5>
-              <div class="mb-3">${date.toLocaleDateString()}</div>
-              <div>
-                ${article.description}
-                <a href=${article.url}>>></a>
-              </div>
-            </div>
-          </div>
-        </div>
-        `;
-    });
-    document.getElementById('feed').innerHTML = content;
-  });
+window.fetch('https://newsapi.org/v1/articles?source=bbc-news&apiKey=7b670b164aa941c88eef1d1350fa4296').then(function (response) {
+  return response.json();
+}).then(function (response) {
+  var articles = response.articles;
 
+  var content = '';
+  articles.forEach(function (article) {
+    var date = new Date(article.publishedAt);
+
+    content += '\n        <div class="row mt-3">\n          <div class="media">\n            <div class="col-4">\n              <a href=' + article.url + '>\n                <img src=' + article.urlToImage + ' width=\'100%\'>\n              </a>\n            </div> \n            <div class="media-body col-8">\n              <h5>' + article.title + '</h5>\n              <div class="mb-3">' + date.toLocaleDateString() + '</div>\n              <div>\n                ' + article.description + '\n                <a href=' + article.url + '>>></a>\n              </div>\n            </div>\n          </div>\n        </div>\n        ';
+  });
+  document.getElementById('feed').innerHTML = content;
+});
 
 /***/ }),
 /* 3 */
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
   output: {
     filename: 'feed.js',
     path: path.resolve(__dirname, 'js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+  ]
   }
 };
